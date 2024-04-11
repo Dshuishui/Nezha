3dbf8a2023a170287068f70cff77d269a35ee355
dyucong <dyucong@outlook.com>
2024-04-11 21:52:16 +0800

    接上者

--- raft/raft.go
+++ raft/raft.go
@@ -583,7 +583,7 @@ func (rf *Raft) appendEntriesLoop() {
 
 			// 100ms广播1次
 			now := time.Now()
-			if now.Sub(rf.lastBroadcastTime) < 38*time.Millisecond {
+			if now.Sub(rf.lastBroadcastTime) < 18*time.Millisecond {
 				return
 			}
 			if rf.lastIndex() == 0 {
