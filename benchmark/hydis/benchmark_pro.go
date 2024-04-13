721cec28bb675e6d6ff9945218f9645b443b0794
dyucong <dyucong@outlook.com>
2024-04-13 11:02:48 +0800

    123

--- benchmark/hydis/benchmark_pro.go
+++ benchmark/hydis/benchmark_pro.go
@@ -177,7 +177,7 @@ func (kvc *KVClient) PutInRaft(key string, value string, pools []pool.Pool) (*kv
 		} else if reply.Err == raft.ErrWrongLeader {
 			kvc.changeToLeader(int(reply.LeaderId))
 			fmt.Printf("等待leader的出现,更改后的leaderid是%v\n",kvc.leaderId)
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(1000 * time.Millisecond)
 		}
 	}
 }
