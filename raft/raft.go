adbde1005d0b181d9457fecb60b04eaef1d70129
dyucong <dyucong@outlook.com>
2024-04-17 16:47:05 +0800

    1

--- raft/raft.go
+++ raft/raft.go
@@ -521,7 +521,7 @@ func (rf *Raft) sendAppendEntries(address string, args *raftrpc.AppendEntriesInR
 	}
 	defer conn.Close()
 	client := raftrpc.NewRaftClient(conn.Value())
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
 	defer cancel()
 	reply, err := client.AppendEntriesInRaft(ctx, args)
 
