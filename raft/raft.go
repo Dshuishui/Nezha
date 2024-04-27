c45b384045e975f50d8f8e5aa261f4aa1ad16ce8
董水水 <441896483@qq.com>
2024-04-28 00:06:58 +0800

    12

--- raft/raft.go
+++ raft/raft.go
@@ -547,7 +547,7 @@ func (rf *Raft) AppendMonitor() {
 	for {
 		time.Sleep(timeout)
 		if (time.Since(rf.LastAppendTime) > timeout) && rf.GetLeaderId() != int32(rf.me) {
-			fmt.Println("5秒没有收到来自leader的同步或者心跳信息！")
+			fmt.Println("3秒没有收到来自leader的同步或者心跳信息！")
 			continue
 		}
 		fmt.Printf("当前的log大小%v\n", rf.lastIndex())
