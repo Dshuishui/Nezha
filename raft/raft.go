107832281695d5bc66bd9ce96f2530d68285f491
dyucong <dyucong@outlook.com>
2024-05-07 10:21:26 +0800

    12

--- raft/raft.go
+++ raft/raft.go
@@ -1293,7 +1293,7 @@ func Make(peers []string, me int,
 	rf.persister = persister
 	rf.me = me
 	for i := 0; i < 3; i++ {
-		rf.SyncChans = append(rf.SyncChans, make(chan string, 600))
+		rf.SyncChans = append(rf.SyncChans, make(chan string, 100))
 	}
 
 	rf.role = ROLE_FOLLOWER
