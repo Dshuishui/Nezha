274fd807552d80ddad2ac51b0d1793a8d879e300
董水水 <441896483@qq.com>
2024-05-08 23:56:11 +0800

    还原

--- raft/persister.go
+++ raft/persister.go
@@ -2,8 +2,8 @@ package raft
 import (
 	"github.com/JasonLou99/Hybrid_KV_Store/util"
 
-	// "github.com/syndtr/goleveldb/leveldb"
-	"github.com/tecbot/gorocksdb"
+	"github.com/syndtr/goleveldb/leveldb"
+	// "github.com/tecbot/gorocksdb"
 )
 
 import "sync"
@@ -12,8 +12,8 @@ type Persister struct {
 	mu        sync.Mutex
 	raftstate []byte
 	snapshot  []byte
-	// db *leveldb.DB
-	db *gorocksdb.DB
+	db *leveldb.DB
+	// db *gorocksdb.DB
 }
 
 
@@ -21,57 +21,57 @@ func (p *Persister) Init(path string) {
 	var err error
 	// 数据存储路径和一些初始文件
 	// 打开指定路径的LevelDB数据库文件，并将返回的leveldb.DB实例赋值给p.db字段，同时将可能的错误赋值给err变量。
-	// p.db, err = leveldb.OpenFile(path, nil)
-	// if err != nil {
-	// 	util.EPrintf("Open db failed, err: %s", err)
-	// }
-	bbto := gorocksdb.NewDefaultBlockBasedTableOptions()
-	bbto.SetBlockCache(gorocksdb.NewLRUCache(3 << 30))
-	opts := gorocksdb.NewDefaultOptions()
-
-	opts.SetBlockBasedTableFactory(bbto)
-	opts.SetCreateIfMissing(true)
-	p.db, err = gorocksdb.OpenDb(opts, path)
+	p.db, err = leveldb.OpenFile(path, nil)
 	if err != nil {
 		util.EPrintf("Open db failed, err: %s", err)
 	}
+	// bbto := gorocksdb.NewDefaultBlockBasedTableOptions()
+	// bbto.SetBlockCache(gorocksdb.NewLRUCache(3 << 30))
+	// opts := gorocksdb.NewDefaultOptions()
+
+	// opts.SetBlockBasedTableFactory(bbto)
+	// opts.SetCreateIfMissing(true)
+	// p.db, err = gorocksdb.OpenDb(opts, path)
+	// if err != nil {
+	// 	util.EPrintf("Open db failed, err: %s", err)
+	// }
 }
 
 func (p *Persister) Put(key string, value []byte) {
-	// err := p.db.Put([]byte(key), []byte(value), nil)	// 转换成字节数组是因为LevelDB是只接受字节数组作为键和值的输入。
-	// if err != nil {
-	// 	util.EPrintf("Put key %s value %s failed, err: %s", key, value, err)
-	// }
-	wo := gorocksdb.NewDefaultWriteOptions()
-	defer wo.Destroy()
-	err := p.db.Put(wo, []byte(key), value)
+	err := p.db.Put([]byte(key), []byte(value), nil)	// 转换成字节数组是因为LevelDB是只接受字节数组作为键和值的输入。
 	if err != nil {
 		util.EPrintf("Put key %s value %s failed, err: %s", key, value, err)
 	}
+	// wo := gorocksdb.NewDefaultWriteOptions()
+	// defer wo.Destroy()
+	// err := p.db.Put(wo, []byte(key), value)
+	// if err != nil {
+	// 	util.EPrintf("Put key %s value %s failed, err: %s", key, value, err)
+	// }
 }
 
-// func (p *Persister) Get(key string) []byte {
-// 	value, err := p.db.Get([]byte(key), nil)
-// 	if err != nil {
-// 		util.EPrintf("Get key %s failed, err: %s", key, err)
-// 		return nil	// 因为有返回值，所有需要return
-// 	}
-// 	return value
-// }
-
-func (p *Persister) Get(key string) ([]byte, error) {
-	ro := gorocksdb.NewDefaultReadOptions()
-	defer ro.Destroy()
-
-	value, err := p.db.Get(ro, []byte(key))
+func (p *Persister) Get(key string) []byte {
+	value, err := p.db.Get([]byte(key), nil)
 	if err != nil {
 		util.EPrintf("Get key %s failed, err: %s", key, err)
-		return nil, err
+		return nil	// 因为有返回值，所有需要return
 	}
-	defer value.Free()
-
-	return value.Data(), nil
+	return value
 }
+
+// func (p *Persister) Get(key string) ([]byte, error) {
+// 	ro := gorocksdb.NewDefaultReadOptions()
+// 	defer ro.Destroy()
+
+// 	value, err := p.db.Get(ro, []byte(key))
+// 	if err != nil {
+// 		util.EPrintf("Get key %s failed, err: %s", key, err)
+// 		return nil, err
+// 	}
+// 	defer value.Free()
+
+// 	return value.Data(), nil
+// }
 
 func MakePersister() *Persister {
 	return &Persister{}
