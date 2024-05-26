e087ed947be3cea0a21ba2b4df4e094d514697fe
董水水 <441896483@qq.com>
2024-05-26 15:56:54 +0800

    测试read，设置自动关闭服务器时间为38000s

--- kvstore/LevelDB/LevelDB.go
+++ kvstore/LevelDB/LevelDB.go
@@ -650,7 +650,7 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	go kvs.RegisterKVServer(ctx, kvs.address)
 	go func() {
-		timeout := 38 * time.Second
+		timeout := 38000 * time.Second
 		for {
 			time.Sleep(timeout)
 			// if (time.Since(kvs.lastPutTime) > timeout) && (time.Since(kvs.raft.LastAppendTime) > timeout) {
