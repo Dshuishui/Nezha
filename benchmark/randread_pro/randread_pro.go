8940f00b5ed3b28a4fba01717b5a8acec082160b
董水水 <441896483@qq.com>
2024-10-16 10:47:58 +0800

    这样测还可以，读取的key随机性较大

--- benchmark/randread_pro/randread_pro.go
+++ benchmark/randread_pro/randread_pro.go
@@ -56,7 +56,7 @@ func (kvc *KVClient) randRead() {
 			localResult := getResult{}
 			rand.Seed(time.Now().UnixNano())
 			for j := 0; j < base; j++ {
-				key := rand.Intn(1000000)
+				key := rand.Intn(100000000)
 				//k := base*i + j
 				// key := fmt.Sprintf("key_%d", k)
 				targetkey := strconv.Itoa(key)
