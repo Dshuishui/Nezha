557d9957d159d10498e7ed7012b3fe87493f4c84
董水水 <441896483@qq.com>
2025-01-16 09:44:36 +0800

    12

--- kvstore/FlexSync/FlexSync.go
+++ kvstore/FlexSync/FlexSync.go
@@ -1645,7 +1645,7 @@ func main() {
 	// ctx, _ := context.WithCancel(context.Background())
 	go kvs.RegisterKVServer(ctx, kvs.address)
 	go func() {
-		timeout := 4 * time.Second
+		timeout := 400000 * time.Second
 		time1 := 500000 * time.Second
 		for {
 			time.Sleep(timeout)
